2f826094272e78c6371ac5372a08c043cfebec97
vpapudesibabu <vpapudesibabu@hawk.iit.edu>
2023-11-29 00:58:13 -0600

    metronome to watch blocks created by validator

--- checkpoint2/metronome.py
+++ checkpoint2/metronome.py
@@ -7,7 +7,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from block import Block, Transaction
 import config_validator
 
-
 # Set up the logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -28,6 +27,8 @@ template = {
         'threads': int
     }
 }
+
+ack_received = False
 
 validator_config, err = config_validator.get_validated_fields(
     'dsc-config.yaml', template)
@@ -51,6 +52,13 @@ def dif():
     difficulty = response.json()['difficulty_bits']
     return {'difficulty': difficulty}
 
+
+@app.route('/block/ack')
+def block_ack():
+    global ack_received
+    ack_received = True
+    print(f'Received an ACK from validator upon block creation')
+    return {'received': 'OK'}
 
 @app.route('/nonce')
 def nonce():
@@ -84,8 +92,9 @@ def create_block():
     url = 'http://{0}:{1}/lastblock'.format(cfg_bc['server'], cfg_bc['port'])
     res = requests.get(url)
 
-    block = Block(version=int(config["version"]), prev_block_hash=res.json()['block'], block_id=res.json()['block_id'], timestamp=int(
-        time.time()), difficulty_target=30, nonce=123456, transactions=[])
+    block = Block(version=int(config["version"]), prev_block_hash=res.json()['block'], block_id=res.json()['block_id'],
+                  timestamp=int(
+                      time.time()), difficulty_target=30, nonce=123456, transactions=[])
 
     # Serialize and Deserialize Block
     serialized_block = block.pack()
@@ -95,5 +104,15 @@ def create_block():
         f"New block created, hash {block.calculate_hash()} sent to blockchain")
 
 
-scheduler.add_job(create_block, 'interval', seconds=6)
+def watcher():
+    timeout_duration = 6
+    start_time = time.time()
+    ack_received = False
+    while time.time() - start_time < timeout_duration:
+        if ack_received:  # will be true if validator ack's
+            time.sleep(timeout_duration - time.time() - start_time)
+    if not ack_received:
+        create_block()
+
+scheduler.add_job(watcher, 'interval', seconds=7)
 scheduler.start()
