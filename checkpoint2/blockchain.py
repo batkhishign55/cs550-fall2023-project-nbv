d3dc246605b795cee2c8832af1177890740e0fbc
vpapudesibabu <vpapudesibabu@hawk.iit.edu>
2023-11-30 14:08:21 -0600

    Revert "dynamic diff bits calculation"
    This reverts commit 058afb60e6efd608551192de4d3eb324ae510587.

--- checkpoint2/blockchain.py
+++ checkpoint2/blockchain.py
@@ -1,5 +1,4 @@
 import datetime
-import logging
 import time
 
 import requests
@@ -7,16 +6,6 @@ from flask import Flask, request
 import yaml
 
 from block import Block, Transaction
-# Set up the logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter(
-    '%(asctime)s.%(msecs)03d %(message)s', datefmt='%Y%m%d %H:%M:%S')
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
 
 app_info = "DSC v1.0"
 
@@ -44,7 +33,7 @@ class Blockchain:
     def __init__(self):
         self.blocks = []
         self.difficulty_bits = 30
-        self.difficulty_tracker = {'last-block': None, 'counter': 0}
+        # self.difficulty_tracker = {'last-block': None, 'counter': 0}
         # self.consecutive_validator_blocks = 0
         # self.consecutive_metronome_blocks = 0
 
@@ -60,7 +49,7 @@ class Blockchain:
     def get_balance(self, wallet, cache_block_height, cache_bal):
         balance = cache_bal
         for block in self.blocks[cache_block_height + 1:]:
-            # print("searched in a block")
+            print("searched in a block")
             for txn in block.transactions:
                 if txn.recipient_address == wallet:
                     balance += txn.value
@@ -82,26 +71,22 @@ def hello():
 @app.post('/addblock')
 def addblock():
     block = Block.unpack(request.data)
-    if (blockchain.get_last_block_hash() != block.prev_block_hash
-            and block.prev_block_hash != blockchain.difficulty_tracker['last-block']):
-        blockchain.add_block(block)
-        blockchain.difficulty_tracker = {'last-block': block.prev_block_hash, 'counter': 1}
+    # if blockchain.get_last_block_hash() == block.prev_block_hash:
+    blockchain.add_block(block)
+    blockchain.difficulty_tracker = {'last-block': block.prev_block_hash, 'counter': 1}
+    print(f"The block is {block.block_id, block.nonce, block.timestamp, block.transactions}")
+    print(f"Sending below transactions to cleanup {block.transactions}")
+    url = 'http://{0}:{1}/confirmed_transactions'.format(
+        cfg_pool['server'], cfg_pool['port'])
 
-        # print(f"The block is {block.block_id, block.nonce, block.timestamp, block.transactions}")
-        # print(f"Sending below transactions to cleanup {block.transactions}")
-        url = 'http://{0}:{1}/confirmed_transactions'.format(
-            cfg_pool['server'], cfg_pool['port'])
-
-        x = requests.post(url, data=request.data)
-        received_from = "metronome"
-        if len(block.transactions) != 0:
-            received_from = "validator"
-        logger.info(f" New block received from {received_from}, Block hash {block.calculate_hash()}")
-        # elif blockchain.difficulty_tracker['last-block'] != block.prev_block_hash:
-        #     blockchain.difficulty_tracker['counter'] += 1
-    elif block.prev_block_hash == blockchain.difficulty_tracker['last-block']:
-        blockchain.difficulty_tracker['counter'] += 1
-
+    x = requests.post(url, data=request.data)
+    received_from = "metronome"
+    if len(block.transactions) != 0:
+        received_from = "validator"
+    print(datetime.datetime.now(
+    ), f" New block received from {received_from}, Block hash {block.calculate_hash()}")
+    # elif blockchain.difficulty_tracker['last-block'] != block.prev_block_hash:
+    #     blockchain.difficulty_tracker['counter'] += 1
 
     return {"message": "success"}
 
@@ -125,7 +110,8 @@ def balance():
         print(request.args["wallet"], balance, block_height)
         cache.update_cache(request.args["wallet"], balance, block_height)
 
-    logger.info(f"Balance request for {request.args['wallet']} , {str(balance)} coins")
+    print(datetime.datetime.now(), " Balance request for " +
+          request.args["wallet"] + ", " + str(balance) + " coins")
     return {"balance": balance}
 
 
@@ -139,24 +125,25 @@ def get_cache():
 # curl "localhost:10002/txn?id=some-txn-id"
 @app.get('/txn')
 def transaction():
-    logger.info(f"Transaction request status for {request.args['id']} unknown")
+    print(datetime.datetime.now(), " Transaction request status for " +
+          request.args["id"] + ", unkhown")
     return "unknown"
 
 
 # curl "localhost:10002/txns?ids=id1,id2,id3"
 @app.get('/txns')
 def transactions():
-    logger.info(f"Transactions request for {request.args['ids']} none found")
+    print(datetime.datetime.now(), " Transactions request for " +
+          request.args["ids"] + ", none found")
     return "none found"
 
 
 @app.get('/difficulty')
 def difficulty():
-    if validator_instances > 12: # as it will be checked only when there are 12 validators
-        if blockchain.difficulty_tracker['counter'] > 8:
-            blockchain.difficulty_bits += 1
-        elif blockchain.difficulty_tracker['counter'] < 4:
-            blockchain.difficulty_bits -= 1
+    # if blockchain.difficulty_tracker['counter'] > 0.75 * validator_instances:
+    #     blockchain.difficulty_bits += 1
+    # elif blockchain.difficulty_tracker['counter'] < 0.25 * validator_instances:
+    #     blockchain.difficulty_bits -= 1
     return {"difficulty_bits": blockchain.difficulty_bits}
 
 
@@ -178,8 +165,8 @@ def create_genesis_block():
 
 
 config = load_config()
-logger.info(f" DSC {str(config['version'])}")
-logger.info("Blockchain server started with 2 worker threads")
+print(datetime.datetime.now(), " DSC " + str(config["version"]))
+print(datetime.datetime.now(), " Blockchain server started with 2 worker threads")
 difficulty_bits = 30
 create_genesis_block()
 cfg_pool = config['pool']
